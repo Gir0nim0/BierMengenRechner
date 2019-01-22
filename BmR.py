1da03c7e313d5c503307e964a23701c9110493ff
Jan Jürgens <46740123+Gir0nim0@users.noreply.github.com>
2019-01-22 21:00:13 +0100

    Add files via upload

--- BmR.py
+++ BmR.py
@@ -5,9 +5,10 @@ def buttonBerechnenClick():
     klein = int(entryFlascheKlein.get())
     mittel = int(entryFlascheMittel.get())
     gross = int(entryFlascheGross.get())
+    maxi = int(entryFlascheMaxi.get())
 
     # Verarbeitung der Daten
-    LiterWert = (klein * 0.33) + (mittel * 0.5) + (gross * 0.75)
+    LiterWert = (klein * 0.33) + (mittel * 0.5) + (gross * 0.75) + maxi
     LiterWert = round(LiterWert, 2)
     # Anzeige der Daten
     labelLiterWert.config(text = LiterWert)
@@ -22,10 +23,13 @@ def loeschen_entryFlascheMittel(event):
 def loeschen_entryFlascheGross(event):
     entryFlascheGross.delete(0, END)
 
+def loeschen_entryFlascheMaxi(event):
+    entryFlascheMaxi.delete(0, END)
+
 # Fenster
 tkFenster = Tk()
 tkFenster.title('BierMengenRechner')
-tkFenster.geometry('250x250')
+tkFenster.geometry('300x300')
 
 # Label mit Aufschrift 0,33L Flaschen
 labelFlascheKlein = Label(master=tkFenster, text='Wieviele 0,33L Flaschen?')
@@ -38,6 +42,10 @@ labelFlascheMittel.place(x=30, y=64, width=130, height=27)
 # Label mit Aufschrift 0,75L Flaschen
 labelFlascheGross = Label(master=tkFenster, text='Wieviele 0,75L Flaschen?')
 labelFlascheGross.place(x=30, y=104, width=130, height=27)
+
+# Label mit Aufschrift 1L Flaschen
+labelFlascheMaxi = Label(master=tkFenster, text='Wieviele 1L Flaschen?')
+labelFlascheMaxi.place(x=30, y=144, width=130, height=27)
 
 # Entry für 0,33L Flaschen
 entryFlascheKlein = Entry(master=tkFenster, bg='white')
@@ -57,22 +65,28 @@ entryFlascheGross.place(x=164, y=104, width=40, height=27)
 entryFlascheGross.insert(0,'0')
 entryFlascheGross.bind('<Button-1>', loeschen_entryFlascheGross)
 
+# Entry für 1L Flaschen
+entryFlascheMaxi = Entry(master=tkFenster, bg='white')
+entryFlascheMaxi.place(x=164, y=144, width=40, height=27)
+entryFlascheMaxi.insert(0,'0')
+entryFlascheMaxi.bind('<Button-1>', loeschen_entryFlascheMaxi)
+
 # Button zum Berechnen
-buttonBerechnen = Button(master=tkFenster, bg='#e5e5e5', text='Biermenge berechnen', command=buttonBerechnenClick)
-buttonBerechnen.place(x=30, y=144, width=130, height=27)
+buttonBerechnen = Button(master=tkFenster, bg='#e5e5e5', wraplength=150, text='abgefüllte Biermenge berechnen', command=buttonBerechnenClick)
+buttonBerechnen.place(x=30, y=184, width=130, height=40)
 
 # Label mit Aufschrift Liter
 labelLiter = Label(master=tkFenster, text='Liter')
-labelLiter.place(x=170, y=144, width=100, height=27)
+labelLiter.place(x=170, y=190, width=100, height=27)
 
 # Label für den Liter Wert
-labelLiterWert = Label(master=tkFenster, text='')
-labelLiterWert.place(x=164, y=144, width=40, height=27)
+labelLiterWert = Label(master=tkFenster, text='', fg="blue")
+labelLiterWert.place(x=164, y=190, width=40, height=27)
 
 # Label Info
 
-labelInfo = Label(master=tkFenster, anchor=CENTER, wraplength=200, fg="blue", text='Dies ist ein kleines Tool zum berechnen der Biermenge anhand der abgefüllten Flaschen.')
-labelInfo.place(x=20, y=190, width=200, height=50)
+labelInfo = Label(master=tkFenster, anchor=CENTER, wraplength=200, fg="red", text='Dies ist ein kleines Tool zum berechnen der Biermenge anhand der abgefüllten Flaschen.')
+labelInfo.place(x=50, y=240, width=200, height=50)
 
 # Aktivierung des Fensters
 tkFenster.mainloop()
